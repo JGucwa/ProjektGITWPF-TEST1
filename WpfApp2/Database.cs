659b372cfa71c1f0fe782d1c3803636ef36aa42f
JGucwa <kubjaxxx@gmail.com>
2024-01-15 10:42:54 +0100

    test

---
+++ WpfApp2/Database.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Database
+    {
+        readonly string FilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "baza.json");
+
+        public List<Zadanie> WszystkieZadania()
+        {
+            if (JsonConvert.DeserializeObject<List<Zadanie>>(FilePath) != null)
+            {
+                string tekst = File.ReadAllText(FilePath);
+                return JsonConvert.DeserializeObject<List<Zadanie>>(tekst);
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+        public void DodajZadanie(Zadanie zadanie)
+        {
+            List<Zadanie> tmp = WszystkieZadania();
+
+            if(tmp != null)
+            {
+                if(tmp.Count == 0) zadanie.Id = 0; else zadanie.Id = tmp[tmp.Count-1].Id + 1;
+                tmp.Add(zadanie);
+
+                string tekst = JsonConvert.SerializeObject(tmp);
+
+                File.WriteAllText(FilePath, tekst);
+            }
+        }
+        public void EdytujZadanie(Zadanie zadanie)
+        {
+            List<Zadanie> tmp = WszystkieZadania();
+
+            if (tmp != null)
+            {
+                for(int i = 0; i < tmp.Count; i++)
+                {
+                    if(zadanie.Id == tmp[i].Id)
+                    {
+                        tmp[i] = zadanie;
+                    }
+                }
+
+                string tekst = JsonConvert.SerializeObject(tmp);
+
+                File.WriteAllText(FilePath, tekst);
+            }
+        }
+    }
+}
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -16,9 +16,69 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        ItemsControl Lista;
         public MainWindow()
         {
             InitializeComponent();
+
+            SortujZadania();
         }
+        private void SortujZadania()
+        {
+            List<Zadanie> tmp = new List<Zadanie>();
+            for (int i = 0;i < new Database().WszystkieZadania().Count;i++)
+            {
+                if (new Database().WszystkieZadania()[i].Data == Data.SelectedDate)
+                {
+                    tmp.Add(new Database().WszystkieZadania()[i]);
+                }
+            }
+
+            tmp.Reverse();
+            Lista.ItemsSource = tmp;
+        }
+        public void DodajZadanie(object s, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(Nazwa) && !string.IsNullOrWhiteSpace(Opis))
+            {
+                new Database().DodajZadanie(new Zadanie() { Data = Data.SelectedDate, Nazwa = Nazwa.Text, Opis = Opis.Text, Priorytet = Priorytet.SelectedIndex });
+
+                SortujZadania();
+
+                Nazwa.Text = Opis.Text = String.Empty;
+
+                MessageBox.Show("Pomyślnie dodano zadanie");
+            }
+            else
+            {
+                MessageBox.Show("Podane dane są nieprawidłowe");
+            }
+        }
+        Zadanie zadanieZaznaczone = new Zadanie();
+        public void WyswietlOpis(object s, RoutedEventArgs e)
+        {
+            StronaGlowna.Visibility = Visibility.Collapsed;
+            opis.Visibility = Visibility.Visible;
+
+            MenuItem menu = s as MenuItem;
+            zadanieZaznaczone = menu.CommandParameter as Zadanie;
+
+            Opis.DataContext = zadanieZaznaczone;
+        }
+        public void ZmienPriorytet(object s, RoutedEventArgs e)
+        {
+            //zadanieZaznaczone.Priorytet = NowyPriorytet.SelectedIndex;
+            new Database().EdytujZadanie(zadanieZaznaczone);
+
+            MessageBox.Show("Poprawnie zmieniono priorytet");
+        }
+        public void WyswietlStroneGlowna(object s, RoutedEventArgs e)
+        {
+            StronaGlowna.Visibility = Visibility.Visible;
+            opis.Visibility = Visibility.Collapsed;
+
+            SortujZadania();
+        }
+
     }
 }
